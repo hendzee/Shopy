37837b98b6fc98b29d8d76006138cc9eebd0a9aa
Hendras <hendzcode@gmail.com>
2022-02-16 14:28:35 +0700

    Initial Commit

---
+++ shopy/shopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopyApp.swift
+//  shopy
+//
+//  Created by Virginia Hendras on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct shopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
